cca664447e9ee7087371ab73ed1c0f1e2b625636
Massil <160241880+Massil-br@users.noreply.github.com>
2025-04-01 14:17:09 +0200

    animation player

--- src/Player.cs
+++ src/Player.cs
@@ -1,5 +1,6 @@
 using SFML.Graphics;
 using SFML.System;
+using Shared;
 
 namespace src
 {
@@ -11,6 +12,12 @@ namespace src
         private Vector2f baseAttack;
 
         private const float AttackDuration = 0.1f; // Durée de l'attaque en secondes
+
+        private Sprite sprite;
+
+        private Texture[] palyerTexList;
+
+
     
 
         public Player()
@@ -19,11 +26,19 @@ namespace src
             Shape.FillColor = Color.Blue;
             baseAttack = new Vector2f(590, 600); // Bas de l'écran
             Shape.Position = baseAttack;
+            sprite = new();
+            palyerTexList = [
+                TextureManager.GetTexture("src/assets/Player/PosePlayer.png"),
+                TextureManager.GetTexture("src/assets/Player/AttackPlayer.png")
+            ];
+            sprite.Texture = palyerTexList[0];
+
         }
 
         public void Attack(){
             isAttacking = true;
             baseAttackTimer = AttackDuration;
+
         }
 
         public void Update(float deltaTime)
@@ -35,18 +50,38 @@ namespace src
                 // Déplacement vers l'avant pendant l'attaque
                 float progress = 1 - (baseAttackTimer / AttackDuration); // De 0 à 1
                 Shape.Position = baseAttack * progress;
+                PlayAttackAnimation();
                 // Fin de l'attaque
                 if (baseAttackTimer <= 0)
                 {
                     isAttacking = false;
                     Shape.Position = baseAttack; // Retour à la position initiale
+                    PlayIdleAnimation();
                 }
             }
+
+            UpdatePlayerSpritePosition();
+
+
+        }
+
+        private void PlayAttackAnimation(){
+            sprite.Texture = palyerTexList[1];
+        }
+        private void PlayIdleAnimation(){
+            sprite.Texture = palyerTexList[0];
+        }
+
+        private void UpdatePlayerSpritePosition(){
+            sprite.Position = Shape.Position;
         }
 
         public void Draw(RenderWindow window)
         {
-            window.Draw(Shape);
+           // window.Draw(Shape); // a la place du draw shape tu draw Sprite texture a la position de shape
+            
+
+            window.Draw(sprite);
         }
     }
 }

5f3fa404a7849126294913ab4aaf2247ce882939
mjordan <jordan.milleville@ynov.com>
2025-04-01 13:56:08 +0200

    player png

--- src/Player.cs
+++ src/Player.cs
@@ -6,12 +6,42 @@ namespace src
     public class Player
     {
         public CircleShape Shape;
+        private float baseAttackTimer;
+        private bool isAttacking;
+        private Vector2f baseAttack;
+
+        private const float AttackDuration = 0.1f; // Durée de l'attaque en secondes
+    
 
         public Player()
         {
             Shape = new CircleShape(50f); // rayon 50
             Shape.FillColor = Color.Blue;
-            Shape.Position = new Vector2f(590, 600); // Bas de l'écran
+            baseAttack = new Vector2f(590, 600); // Bas de l'écran
+            Shape.Position = baseAttack;
+        }
+
+        public void Attack(){
+            isAttacking = true;
+            baseAttackTimer = AttackDuration;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (isAttacking)
+            {
+                baseAttackTimer -= deltaTime;
+
+                // Déplacement vers l'avant pendant l'attaque
+                float progress = 1 - (baseAttackTimer / AttackDuration); // De 0 à 1
+                Shape.Position = baseAttack * progress;
+                // Fin de l'attaque
+                if (baseAttackTimer <= 0)
+                {
+                    isAttacking = false;
+                    Shape.Position = baseAttack; // Retour à la position initiale
+                }
+            }
         }
 
         public void Draw(RenderWindow window)
